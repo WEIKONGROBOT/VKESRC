64a04d0b95521a5d7825edc3fa0b033e0f0d2258
fjpaxkm <jpfeng@foxmail.com>
2020-09-01 09:43:10 +0800

    add omni code
    add omni code

--- arduino/vkesrc_arduino_library_file/Arduino15/packages/VKESRC/hardware/VKESRC/1.0.0/libraries/transbot/examples/transbot_friends/transbot_omni/transbot_omni.ino
+++ arduino/vkesrc_arduino_library_file/Arduino15/packages/VKESRC/hardware/VKESRC/1.0.0/libraries/transbot/examples/transbot_friends/transbot_omni/transbot_omni.ino
@@ -24,15 +24,15 @@
 HardwareTimer Timer(TIMER_CH1);
 
 /*******************************************************************************
-* Declaration for RC100 remote conroller
+* Declaration for VKPS2 remote conroller
 *******************************************************************************/
-RC100 remote_controller;
+VKPS2 remote_controller;
 double const_cmd_vel    = 0.2;
 
 /*******************************************************************************
 * Declaration for motor
 *******************************************************************************/
-Turtlebot3MotorDriver motor_driver;
+TransbotMotorDriver motor_driver;
 
 double linear_x                = 0.0;
 double linear_y                = 0.0;
@@ -43,13 +43,21 @@ double goal_angular_velocity   = 0.0;
 
 void setup()
 {
+  Serial.begin(115200);
   // Setting for Dynamixel motors
   motor_driver.init();
 
-  // Setting for RC100 remote control and cmd_vel
-  remote_controller.begin(1);  //57600bps for RC100
+  // Setting for VKPS2 remote control and cmd_vel
+  remote_controller.begin(1);  //9600bps for VKPS2
 
   pinMode(13, OUTPUT);
+  
+//change by jiapengfeng for test 20200825
+  pinMode(BDPIN_DIP_SW_1, INPUT);
+  pinMode(BDPIN_DIP_SW_2, INPUT);
+  pinMode(BDPIN_PUSH_SW_1, INPUT);
+  pinMode(BDPIN_PUSH_SW_2, INPUT);
+//change by jiapengfeng for test 20200825
 
   SerialBT2.begin(57600);
 
@@ -66,106 +74,194 @@ void startDynamixelControlInterrupt()
 {
   Timer.pause();
   Timer.setPeriod(CONTROL_PERIOD);           // in microseconds
-  Timer.attachInterrupt(controlOmni);
+  Timer.attachInterrupt(controlomni);
   Timer.refresh();
   Timer.resume();
 }
 
 /*******************************************************************************
-* Receive RC100 remote controller data
+* Receive VKPS2 remote controller data
 *******************************************************************************/
 void receiveRemoteControl(void)
 {
   int received_data = 0;
+/* 
+//change by jiapengfeng for test 20200825
+  int dip_state;
+  int push_state;
+ 
+  dip_state  = digitalRead(BDPIN_DIP_SW_1)<<0;
+  dip_state |= digitalRead(BDPIN_DIP_SW_2)<<1;
 
+  push_state  = digitalRead(BDPIN_PUSH_SW_1)<<0;
+  push_state |= digitalRead(BDPIN_PUSH_SW_2)<<1;
+
+ // Serial.print("dip_state = ");
+ // Serial.print(dip_state, BIN);
+
+ // Serial.print("\tpush_state = ");
+ // Serial.print(push_state, BIN);
+//X
+    if ((push_state == 2) && (dip_state ==2))
+    {
+      linear_x  = MAX_LINEAR_VELOCITY;
+    }
+    else if ((push_state == 1) &&(dip_state ==2))
+    {
+      linear_x  = (-MAX_LINEAR_VELOCITY);
+    }
+//Y
+    if ((push_state == 1) && (dip_state ==1))
+    {
+      linear_y = (-MAX_LINEAR_VELOCITY);
+    }
+    else if ((push_state == 2) && (dip_state ==1))
+    {
+      linear_y = MAX_LINEAR_VELOCITY;
+    }
+//Z    
+    if ((push_state == 2) && (dip_state ==0))
+    {
+      angular_z = MAX_ANGULAR_VELOCITY;
+    }
+    else if ((push_state == 1) && (dip_state ==0))
+    {
+      angular_z = (-MAX_ANGULAR_VELOCITY);
+    }
+//  Serial.print("  linear_x : ");  Serial.print(linear_x);
+//  Serial.print(" linear_y : "); Serial.print(linear_y);
+//  Serial.print(" angular_z : "); Serial.println(angular_z);
+//change by jiapengfeng for test 20200825
+*/
   if (remote_controller.available())
   {
     received_data = remote_controller.readData();
-
-    if (received_data & RC100_BTN_U)
+    Serial.print("received_data = ");
+    Serial.print(received_data);
+    if (received_data == VKPS2_BTN_U)
     {
       linear_x  += VELOCITY_LINEAR_X * SCALE_VELOCITY_LINEAR_X;
+      Serial.println("VKPS2_BTN_U");
     }
-    else if (received_data & RC100_BTN_D)
+    else if (received_data == VKPS2_BTN_D)
     {
       linear_x  -= VELOCITY_LINEAR_X * SCALE_VELOCITY_LINEAR_X;
+      Serial.println("VKPS2_BTN_D");
     }
-
-    if (received_data & RC100_BTN_L)
+    else if (received_data == VKPS2_BTN_L)
     {
       linear_y -= VELOCITY_LINEAR_Y * SCALE_VELOCITY_LINEAR_Y;
+      Serial.println("VKPS2_BTN_L");
     }
-    else if (received_data & RC100_BTN_R)
+    else if (received_data == VKPS2_BTN_R)
     {
       linear_y += VELOCITY_LINEAR_Y * SCALE_VELOCITY_LINEAR_Y;
+      Serial.println("VKPS2_BTN_R");
     }
-
-    if (received_data & RC100_BTN_1)
-    {
-
-    }
-    else if (received_data & RC100_BTN_2)
+    else if (received_data == VKPS2_BTN_SQUARE)
     {
       angular_z += VELOCITY_ANGULAR_Z * SCALE_VELOCITY_ANGULAR_Z;
+      Serial.println("VKPS2_BTN_SQUARE");
     }
-    else if (received_data & RC100_BTN_3)
-    {
-
-    }
-    else if (received_data & RC100_BTN_4)
+    else if (received_data == VKPS2_BTN_CIRCLE)
     {
       angular_z -= VELOCITY_ANGULAR_Z * SCALE_VELOCITY_ANGULAR_Z;
+      Serial.println("VKPS2_BTN_CIRCLE");
     }
-
-    if (received_data & RC100_BTN_6)
+    else if (received_data == VKPS2_BTN_TRIANGLE)
     {
       linear_x  = const_cmd_vel;
       linear_y  = 0.0;
       angular_z = 0.0;
+      Serial.println("VKPS2_BTN_TRIANGLE");
     }
-    else if (received_data & RC100_BTN_5)
+    else if (received_data == VKPS2_BTN_CROSS)
     {
       linear_x  = 0.0;
       linear_y  = 0.0;
       angular_z = 0.0;
+      Serial.println("VKPS2_BTN_CROSS");
     }
 
     if (linear_x > MAX_LINEAR_VELOCITY)
     {
       linear_x = MAX_LINEAR_VELOCITY;
     }
+    else if((linear_x < (-MAX_LINEAR_VELOCITY)))
+    {
+      linear_x = -MAX_LINEAR_VELOCITY;    
+    }
 
+
+    if (linear_y > MAX_LINEAR_VELOCITY)
+    {
+      linear_y = MAX_LINEAR_VELOCITY;
+    }
+    else if((linear_y < (-MAX_LINEAR_VELOCITY)))
+    {
+      linear_y = -MAX_LINEAR_VELOCITY;    
+    }
+    
     if (angular_z > MAX_ANGULAR_VELOCITY)
     {
       angular_z = MAX_ANGULAR_VELOCITY;
+    }
+    else if((angular_z < (-MAX_ANGULAR_VELOCITY)))
+    {
+      angular_z = -MAX_ANGULAR_VELOCITY;    
     }
 
     goal_linear_x_velocity  = linear_x;
     goal_linear_y_velocity  = linear_y;
     goal_angular_velocity   = angular_z;
+  Serial.print("Vx : ");  Serial.print(goal_linear_x_velocity);
+  Serial.print(" Vy : "); Serial.print(goal_linear_y_velocity);
+  Serial.print(" W : "); Serial.println(goal_angular_velocity);
   }
 }
 
 /*******************************************************************************
-* Control onmi speed
+* Control omni speed
 *******************************************************************************/
-void controlOmni()
+//change by jiapengfeng for test 20200825
+void controlomni()
 {
   bool dxl_comm_result = false;
 
-  int64_t wheel_value[OMNIWHEEL_NUM] = {0, 0, 0};
-  double wheel_angular_velocity[OMNIWHEEL_NUM] = {0.0, 0.0, 0.0};
+  int16_t wheel_value[OMNIWHEEL_NUM] = {0, 0, 0, 0};
+  double wheel_angular_velocity[OMNIWHEEL_NUM] = {0.0, 0.0, 0.0, 0.0};
+  uint8_t wheel_value_data_byte[OMNIWHEEL_NUM][2] = {0,0,0,0,0,0,0,0 };
+  int8_t velocity_direction_flag[OMNIWHEEL_NUM]= {VELOCITY_DIRECTION_LEFT_FRONT_FLAG,VELOCITY_DIRECTION_RIGHT_FRONT_FLAG,VELOCITY_DIRECTION_LEFT_REAR_FLAG,VELOCITY_DIRECTION_RIGHT_REAR_FLAG};
 
-  wheel_angular_velocity[0] = (goal_linear_x_velocity * 0) + (goal_linear_y_velocity * (1 / WHEEL_RADIUS)) + (goal_angular_velocity * (-DISTANCE_CENTER_TO_WHEEL/WHEEL_RADIUS));
-  wheel_angular_velocity[1] = (goal_linear_x_velocity * (sqrt(3) / (2 * WHEEL_RADIUS))) + (goal_linear_y_velocity * (-1 / (2 * WHEEL_RADIUS))) + (goal_angular_velocity * (-DISTANCE_CENTER_TO_WHEEL/WHEEL_RADIUS));
-  wheel_angular_velocity[2] = (goal_linear_x_velocity * (sqrt(3) / (-2 * WHEEL_RADIUS))) + (goal_linear_y_velocity * (-1 / (2 * WHEEL_RADIUS))) + (goal_angular_velocity * (-DISTANCE_CENTER_TO_WHEEL/WHEEL_RADIUS));
+/*
+  wheel_angular_velocity[0] = (1/WHEEL_RADIUS) * (goal_linear_x_velocity - goal_linear_y_velocity - (WHEEL_SEPARATION_X + WHEEL_SEPARATION_Y) * goal_angular_velocity);  //LF
+  wheel_angular_velocity[1] = (1/WHEEL_RADIUS) * (goal_linear_x_velocity + goal_linear_y_velocity + (WHEEL_SEPARATION_X + WHEEL_SEPARATION_Y) * goal_angular_velocity);  //RF
+  wheel_angular_velocity[2] = (1/WHEEL_RADIUS) * (goal_linear_x_velocity + goal_linear_y_velocity - (WHEEL_SEPARATION_X + WHEEL_SEPARATION_Y) * goal_angular_velocity);  //LR
+  wheel_angular_velocity[3] = (1/WHEEL_RADIUS) * (goal_linear_x_velocity - goal_linear_y_velocity + (WHEEL_SEPARATION_X + WHEEL_SEPARATION_Y) * goal_angular_velocity);  //RR
+*/
+  wheel_angular_velocity[0] = (1/WHEEL_RADIUS) * (-COS45 * goal_linear_x_velocity + SIN45 *  goal_linear_y_velocity  + WHEEL_SEPARATION_RADIUS * goal_angular_velocity);  //LR
+  wheel_angular_velocity[1] = (1/WHEEL_RADIUS) * (SIN45 * goal_linear_x_velocity + COS45 *  goal_linear_y_velocity   + WHEEL_SEPARATION_RADIUS * goal_angular_velocity);  //RR
+  wheel_angular_velocity[2] = (1/WHEEL_RADIUS) * (-SIN45 * goal_linear_x_velocity - COS45 *  goal_linear_y_velocity  + WHEEL_SEPARATION_RADIUS * goal_angular_velocity);  //LF
+  wheel_angular_velocity[3] = (1/WHEEL_RADIUS) * (COS45 * goal_linear_x_velocity - SIN45 *  goal_linear_y_velocity   + WHEEL_SEPARATION_RADIUS * goal_angular_velocity);  //RF
 
   for (int id = 0; id < OMNIWHEEL_NUM; id++)
   {
-    wheel_value[id] = wheel_angular_velocity[id] * 9.54 /  RPM_CONSTANT_VALUE;
+    wheel_value[id] = (int16_t)(wheel_angular_velocity[id] * 9.54 / RPM_CONSTANT_VALUE);
 
+    //Match installation direction
+    wheel_value[id] *=  velocity_direction_flag[id];
+    //Limit value range
     if (wheel_value[id] > LIMIT_X_MAX_VALUE)       wheel_value[id] =  LIMIT_X_MAX_VALUE;
     else if (wheel_value[id] < -LIMIT_X_MAX_VALUE) wheel_value[id] = -LIMIT_X_MAX_VALUE;
+    //Match servo data format
+    if(wheel_value[id] < 0)
+    {
+      wheel_value[id] = -wheel_value[id];
+      wheel_value[id] |= (1<<15);
+    }
+    //Match high low bit
+    wheel_value_data_byte[id][0] = DXL_LOBYTE(wheel_value[id]);
+    wheel_value_data_byte[id][1] = DXL_HIBYTE(wheel_value[id]); 
   }
 
 #ifdef DEBUG
@@ -173,8 +269,8 @@ void controlOmni()
   Serial.print(" Vy : "); Serial.print(goal_linear_y_velocity);
   Serial.print(" W : "); Serial.println(goal_angular_velocity);
 #endif
-
-  dxl_comm_result = motor_driver.controlMotor((int64_t)wheel_value[0], (int64_t)wheel_value[1], (int64_t)wheel_value[2]);
+  dxl_comm_result = motor_driver.controlMotor(&wheel_value_data_byte[0][0], &wheel_value_data_byte[1][0], &wheel_value_data_byte[2][0], &wheel_value_data_byte[3][0]);
   if (dxl_comm_result == false)
     return;
 }
+//change by jiapengfeng for test 20200825
